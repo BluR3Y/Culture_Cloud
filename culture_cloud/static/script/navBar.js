58c6f1a482ec3993dc13b4c9309653e855a12a5f
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-01-22 18:41:09 -0500

    redesigned top navigation bar, added side nav bar, and added search box with search animation

---
+++ culture_cloud/static/script/navBar.js
@@ -0,0 +1,99 @@
+
+function displayBackDrop(){     
+    var blur = document.getElementsByClassName("pageBackDrop")[0];     
+    
+    if(!blur.classList.contains("displayBackDrop")){    
+        blur.classList.add("displayBackDrop");     
+    }else{                                           
+        blur.classList.remove("displayBackDrop");   
+    }
+}
+function clickedBackDrop(){         
+    var blur = document.getElementsByClassName("pageBackDrop")[0];     
+    var sideMenu = document.getElementsByClassName("sideMenu")[0];     
+    var menuBtn = document.getElementsByClassName("headerMenuBtn")[0];
+
+    blur.classList.remove("displayBackDrop");       
+
+    if(sideMenu.classList.contains("showSideMenu")){    
+        sideMenu.classList.remove("showSideMenu");  
+    }
+
+    if(menuBtn.classList.contains('openMenu')){
+        menuBtn.classList.remove('openMenu');
+    }
+}
+
+function showSideMenu(){       
+    var sideMenu = document.getElementsByClassName("sideMenu")[0];  
+    var btnCont = document.getElementsByClassName("headerMenuBtn")[0];  
+
+    if(!btnCont.classList.contains('openMenu')){    
+        btnCont.classList.add('openMenu');          
+    }else{                                         
+        btnCont.classList.remove('openMenu');       
+    }
+
+    if(!sideMenu.classList.contains("showSideMenu")){   
+        sideMenu.classList.add("showSideMenu");         
+    }else{                                              
+        sideMenu.classList.remove("showSideMenu");      
+    }
+    displayBackDrop();      
+}
+
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = period;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.placeholder = this.txt;
+
+    var that = this;
+    var delta = 80;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 300;
+    }
+
+    if(this.toRotate[i] == this.toRotate[this.toRotate.length-1] && this.isDeleting){
+        return;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+function searchAnimation(){
+    var searchBar = document.getElementsByClassName("searchInput")[0];
+    var searchExamples = ["Russian Cuisine", "Mexico Beaches", "Hong Kong Attractions", "Thailand Museums"];
+
+    new TxtType(searchBar, searchExamples, 1000);
+}
+
+window.addEventListener('load', function() {
+    searchAnimation();
+})
